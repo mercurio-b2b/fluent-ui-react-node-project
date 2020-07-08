638932fdbc0939aa78f816f4c5a0bc1af36ee7a7
mercurio-b2b <stefan.thomas@idaplan.com>
2020-07-08 17:12:54 -0500

    First file commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of users
+require("./app/routes/user.routes.js")(app);
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
